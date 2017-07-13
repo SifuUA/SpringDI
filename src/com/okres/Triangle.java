97dda3058cdab09dbca960ff604530fef62952dc
SifuUA <alekseykres@gmail.com>
2017-07-13 23:12:13 +0300

    Initial commit

---
+++ src/com/okres/Triangle.java
@@ -0,0 +1,21 @@
+package com.okres;
+
+/**
+ * Created by Alex on 13.07.2017.
+ */
+public class Triangle {
+
+    private String type;
+
+    public void draw(){
+        System.out.println(getType() + " Triangle drow");
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
