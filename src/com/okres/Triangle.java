13e29ed2a90fe0e8ef715a380db1b8bed8d3ec17
SifuUA <alekseykres@gmail.com>
2017-07-14 00:57:20 +0300

    convert to List of points

--- src/com/okres/Triangle.java
+++ src/com/okres/Triangle.java
@@ -1,42 +1,26 @@
 package com.okres;
 
+import java.util.List;
+
 /**
  * Created by Alex on 13.07.2017.
  */
 public class Triangle {
 
-    private Point pointA;
-    private Point pointB;
-    private Point pointC;
+    private List<Point> points;
+
+    public List<Point> getPoints() {
+        return points;
+    }
+
+    public void setPoints(List<Point> points) {
+        this.points = points;
+    }
 
     public void draw(){
-        System.out.println(" x = " + getPointA().getX() + " y = " + getPointA().getY());;
-        System.out.println(" x = " + getPointB().getX() + " y = " + getPointB().getY());;
-        System.out.println(" x = " + getPointC().getX() + " y = " + getPointC().getY());;
+        for (Point p : points) {
+            System.out.println(p.getX() + " " + p.getY());
+        }
 
-    }
-
-    public Point getPointA() {
-        return pointA;
-    }
-
-    public void setPointA(Point pointA) {
-        this.pointA = pointA;
-    }
-
-    public Point getPointB() {
-        return pointB;
-    }
-
-    public void setPointB(Point pointB) {
-        this.pointB = pointB;
-    }
-
-    public Point getPointC() {
-        return pointC;
-    }
-
-    public void setPointC(Point pointC) {
-        this.pointC = pointC;
     }
 }
